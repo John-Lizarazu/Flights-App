0037a10858893fbbb305498e58ce9483848099e8
john lizarazu <johnthecreator@johns-mbp.myfiosgateway.com>
2021-03-15 18:57:08 -0400

    used material-ui dates to handle inboundDates

--- src/components/InboundInfo.jsx
+++ src/components/InboundInfo.jsx
@@ -3,5 +3,36 @@ import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles((theme) => ({
-    
-}))
+    container: {
+        display: "flex",
+        flexWrap: "wrap",
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: 300,
+    },
+}));
+
+export default function InboundInfo ({
+    InboundDate,handleInboundChange,
+}) {
+    const inboundclass = useStyles();
+
+    return (
+        <form className={inboundclass.container} noValidate>
+            <TextField
+                style={{width: "100%"}}
+                id="date2"
+                label="Inbound Date"
+                type="date"
+                value={InboundDate}
+                onChange={(e) => handleInboundChange(e.target.value)}
+                className={inboundclass.textField}
+                InputLabelProps={{
+                    shrink: true,
+                }}
+            />
+        </form>
+    )
+}
