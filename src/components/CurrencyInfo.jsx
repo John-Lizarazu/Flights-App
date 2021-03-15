e7e150e10c59e3d9dec26c83b3ff256350af13d9
john lizarazu <johnthecreator@johns-mbp.myfiosgateway.com>
2021-03-14 23:03:52 -0400

    Import material-ui,axios and create infos

---
+++ src/components/CurrencyInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { TextField, Grid } from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
+
+export default function CurrencyInfo ({
+
+})
---
+++ src/components/DestinationInfo.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { TextField, Grid } from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
+import axios from "axios";
+
+export default function DestinationInfo({
+    
+})
