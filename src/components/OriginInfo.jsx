a0a3a437b9e4099e80807c0c33e95e440313263f
john lizarazu <johnthecreator@johns-mbp.myfiosgateway.com>
2021-03-15 00:16:00 -0400

    set up origin api and useEffect

--- src/components/OriginInfo.jsx
+++ src/components/OriginInfo.jsx
@@ -1,3 +1,27 @@
 import { Autocomplete } from "@material-ui/lab";
 import axios from "axios";
 
+export default function OriginInfo ({countries,currency}) {
+    const [originPlace, setOriginPlace] = useState("");
+    const [originPlaces, setOriginPlaces] = useState([]);
+    const [originCity, setOriginCity] = useState("");
+    const [originCountry, setOriginCountry] = useState("");
+
+    useEffect(() => {
+        const fetch = async () => {
+          const country = countries.filter((item) => item.Name === originCountry);
+
+          if (country.length > 0 && originCity.length >=2 && currency.length > 0) {
+              const { data } = await axios.get(
+                `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/${country[0].Code}/${currency}/en-US/`,
+                {
+                    params: { query: originCity},
+                    headers: {
+                        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+                        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+                    },
+                }
+              );
+          }
+        
+}})}
