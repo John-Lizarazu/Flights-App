0a133ac05e5c1fb77e0b446a1d3e567a31e132e4
john lizarazu <johnthecreator@johns-mbp.myfiosgateway.com>
2021-03-15 18:45:36 -0400

    created inbound Date and setting it up

---
+++ src/components/InboundInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
+    
+}))
